81c531de485d4ceec6c133eb20c8cfeb5010be14
mkozakk <mich.kozak@proton.me>
2025-02-03 18:54:21 +0100

    basic folder structure

---
+++ src/main/java/com/cryptoexchange/CryptoExchangeApplication.java
@@ -0,0 +1,12 @@
+package com.cryptoexchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoExchangeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoExchangeApplication.class, args);
+    }
+}
