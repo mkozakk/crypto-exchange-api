147f67fb4d182bc5617c45be276ce4961fc1e8c4
mkozakk <mich.kozak@proton.me>
2025-04-06 18:28:33 +0200

    orderbook socket channel per coin

--- src/main/java/com/cryptoexchange/websocket/MarketSocketPublisher.java
+++ src/main/java/com/cryptoexchange/websocket/MarketSocketPublisher.java
@@ -1,7 +1,9 @@
 package com.cryptoexchange.websocket;
 
 import com.cryptoexchange.dto.PriceTick;
+import com.cryptoexchange.event.OrderBookChangedEvent;
 import com.cryptoexchange.event.PriceUpdatedEvent;
+import com.cryptoexchange.service.OrderBookService;
 import org.springframework.context.event.EventListener;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
@@ -10,14 +12,23 @@ import org.springframework.stereotype.Component;
 public class MarketSocketPublisher {
 
     private final SimpMessagingTemplate messagingTemplate;
+    private final OrderBookService orderBookService;
 
-    public MarketSocketPublisher(SimpMessagingTemplate messagingTemplate) {
+    public MarketSocketPublisher(SimpMessagingTemplate messagingTemplate,
+                                 OrderBookService orderBookService) {
         this.messagingTemplate = messagingTemplate;
+        this.orderBookService = orderBookService;
     }
 
     @EventListener
     public void onPriceUpdated(PriceUpdatedEvent event) {
         messagingTemplate.convertAndSend("/topic/price",
                 new PriceTick(event.symbol(), event.price()));
+    }
+
+    @EventListener
+    public void onOrderBookChanged(OrderBookChangedEvent event) {
+        messagingTemplate.convertAndSend("/topic/orderbook/" + event.symbol(),
+                orderBookService.snapshot(event.symbol()));
     }
 }
