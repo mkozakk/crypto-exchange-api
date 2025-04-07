f704fa2ca4f3e907d1b1de2981fe4a26ad23d429
mkozakk <mich.kozak@proton.me>
2025-04-07 21:21:45 +0200

    push orderbook update after each trade

--- src/main/java/com/cryptoexchange/websocket/MarketSocketPublisher.java
+++ src/main/java/com/cryptoexchange/websocket/MarketSocketPublisher.java
@@ -4,9 +4,10 @@ import com.cryptoexchange.dto.PriceTick;
 import com.cryptoexchange.event.OrderBookChangedEvent;
 import com.cryptoexchange.event.PriceUpdatedEvent;
 import com.cryptoexchange.service.OrderBookService;
-import org.springframework.context.event.EventListener;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
 public class MarketSocketPublisher {
@@ -20,13 +21,13 @@ public class MarketSocketPublisher {
         this.orderBookService = orderBookService;
     }
 
-    @EventListener
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT, fallbackExecution = true)
     public void onPriceUpdated(PriceUpdatedEvent event) {
         messagingTemplate.convertAndSend("/topic/price",
                 new PriceTick(event.symbol(), event.price()));
     }
 
-    @EventListener
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT, fallbackExecution = true)
     public void onOrderBookChanged(OrderBookChangedEvent event) {
         messagingTemplate.convertAndSend("/topic/orderbook/" + event.symbol(),
                 orderBookService.snapshot(event.symbol()));
