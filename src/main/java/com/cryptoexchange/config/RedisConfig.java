1e62c0a3f8e541fc29ab513366704490722dbb95
mkozakk <mich.kozak@proton.me>
2025-04-10 21:11:46 +0200

    redis setup

---
+++ src/main/java/com/cryptoexchange/config/RedisConfig.java
@@ -0,0 +1,21 @@
+package com.cryptoexchange.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory connectionFactory) {
+        return new StringRedisTemplate(connectionFactory);
+    }
+
+    @Bean
+    public ObjectMapper redisObjectMapper() {
+        return new ObjectMapper().findAndRegisterModules();
+    }
+}
